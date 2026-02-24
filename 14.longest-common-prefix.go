43ad3e13d4dbd217770f4eb15d1bc34362ab5535
Raman <priyanshu00268@gmail.com>
2026-02-24 14:50:32 +0530

    solved longest prefix sum - beats 100%

--- 14.longest-common-prefix.go
+++ 14.longest-common-prefix.go
@@ -5,39 +5,38 @@
  */
 
 // @lc code=start
-func longestCommonPrefix(strs []string) string {
-   if len(strs) < 2 {
-	return strs[0]
-   }
-   //find the smallest string
-   s := 0
-   ls := len(strs[0]) 
-   for i := 0; i < len(strs); i++ {
-	   if len(strs[i]) < ls {
-		   s = i
-		   ls = len(strs[i])
-	   }
-   }
 
-   //core logic
-   ans := ""
-   t := -1
-   for i := 0; i < len(strs); i++ {
-	   flag := true
-	   j := 0
-	   for j < len(strs[s]) {
-		   if strs[i][j] != strs[s][j] {
-			   t = j
-			   flag = false
-			   break
-		   }
-	   } 
-	   if !flag {
-		  ans = strs[s][:t]  
-		  break
-	   }
-   }
-   return ans
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 1 {
+		return strs[0]
+	}
+    s := 0
+	ls := len(strs[0]) 
+	for i, str := range strs {
+		if len(strs[i]) < ls {
+			ls = len(str)
+			s = i 
+		}
+	}
+	fmt.Println("value of smallest index s: ", s)
+
+	//core logic
+	for i := 0; i < len(strs[s]); i++ {
+		flag := true
+		for j := 0; j < len(strs); j++ {
+			if strs[j][i] != strs[s][i] {
+				flag = false
+				break
+			}
+		}
+		if !flag {
+			return strs[s][:i]
+		}
+		if i == len(strs[s])-1 {
+			return strs[s]
+		}
+	}
+	return ""
 }
 // @lc code=end
 
